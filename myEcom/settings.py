696d39f1d5114cbfdd1fc357c439f52f0a618f20
diksha1012 <trivedidiksha357@gmail.com>
2021-10-01 02:32:26 +0530

    add

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myEcom.settings")//////////////////+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -158,4 +158,4 @@ MEDIA_URL = "/uploads/products/"
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # MEDIA_ROOT = BASE_DIR
-django_heroku.settings((locals))
+# django_heroku.settings((locals))
