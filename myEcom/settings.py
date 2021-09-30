047d6b3d12e42c3490e7a84aacb99a2ccaccc780
diksha1012 <trivedidiksha357@gmail.com>
2021-09-30 23:42:55 +0530

    allowed host

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ch#erifob$-m25fm&c7tcol8j)+7vzbm$($x=$-&e*^s4c+ta!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['menuuq.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
