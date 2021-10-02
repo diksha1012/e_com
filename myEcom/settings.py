a55a1a4740b7952ccfdb19bd7e494da62ce6256e
diksha1012 <trivedidiksha357@gmail.com>
2021-10-02 16:07:12 +0530

    s

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -164,11 +164,11 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if DEBUG:
    STATICFILES_DIRS = [
-          os.path.join(BASE_DIR,"static"),
+          os.path.join(BASE_DIR,"staticfiles"),
 ]
 
 else:
-      STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    #   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
       MEDIA_ROOT = os.path.join(BASE_DIR, '/uploads/products')
 MEDIA_ROOT=BASE_DIR
 
