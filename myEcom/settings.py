cfb69e2bc910dfdd96343bdb6162a3f8e2eb5b92
diksha1012 <trivedidiksha357@gmail.com>
2021-10-04 00:15:52 +0530

    staticfiles

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ch#erifob$-m25fm&c7tcol8j)+7vzbm$($x=$-&e*^s4c+ta!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['mymenuq.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -163,9 +163,9 @@ MEDIA_URL = "/uploads/products/"
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT=BASE_DIR
+MEDIA_ROOT=os.path.join(BASE_DIR,"shop/uploads")
 
 
 
